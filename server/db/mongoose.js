8ca351e1e2af21229d1bc71c21d1bccb3858817e
aitivinayak369 <aitivinayak369@gmail.com>
2017-10-23 05:17:55 +0400

    test7

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 
 var mongoose=require('mongoose');
 mongoose.promise=global.Promise;
-MONGODB_URI='mongodb://<dbuser>:<dbpassword>@ds127105.mlab.com:27105/test1'
-mongoose.connect(MONGODB_URI);
+MONGODB_URI='mongodb://aitivinayak:vinayak!@ds127105.mlab.com:27105/test1'
+mongoose.connect(MONGODB_URI||'mongodb://localhost:27017/data');
 module.exports={ mongoose};
